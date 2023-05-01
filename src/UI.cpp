b192153d822336d8febe816f61b572d70374a886
Alexey <Garriharripuff@gmail.com>
2023-05-01 15:52:21 +0300

    trying colors

--- src/UI.cpp
+++ src/UI.cpp
@@ -24,7 +24,10 @@ WINDOW* UI::create_newwin(int height, int width, int starty, int startx, chtype 
     WINDOW *local_win;
 
 	local_win = newwin(height, width, starty, startx);
+    wattron(local_win, COLOR_PAIR(1));
+    wbkgd(local_win, COLOR_PAIR(1));
 	wborder(local_win, 0, 0, 0, 0, lu_corner, ru_corner, ll_corner, rl_corner);
+    wattroff(local_win, COLOR_PAIR(1));
 	wrefresh(local_win);
 
 	return local_win;
--- src/UI.hpp
+++ src/UI.hpp
@@ -2,6 +2,8 @@
 
 #include <ncurses.h>
 #include <panel.h>
+#include <exception>
+#include <cstring>
 
 namespace YAExplorer
 {
@@ -10,26 +12,45 @@ namespace YAExplorer
 #define curs_normal 1
 #define curs_visible 2
 
+#define APP_NAME "Yet Another Explorer"
+
 class UI
 {
     protected:
 
     UI()
     {
+        //if (has_colors() == false)
+        //    throw new std::exception();
+
         initscr();
         clear();
         refresh();
         noecho();
         curs_set(curs_invesible);
-        //start_color();
+        start_color();
+
+        init_pair(1, COLOR_CYAN, COLOR_BLACK);
 
         int half_width = COLS / 2;
 
-        up_bar = create_newwin(3, COLS, 0, 0);
-        status_bar = create_newwin(3, COLS, LINES - 3, 0);
-        left = create_newwin(LINES - 4, half_width + 1, 2, 0, ACS_LTEE, ACS_RTEE, ACS_LTEE, ACS_RTEE);
-        right = create_newwin(LINES - 4, half_width, 2, half_width, ACS_TTEE, ACS_RTEE, ACS_BTEE, ACS_RTEE);
+        up_bar = newwin(1, COLS, 0, 0);
+        wbkgd(up_bar, COLOR_PAIR(1));
 
+        mvwprintw(up_bar, 0, COLS - strlen(APP_NAME) - 1, "%s", APP_NAME);
+        wrefresh(up_bar);
+    
+        status_bar = create_newwin(3, COLS, LINES - 3, 0);
+        left = create_newwin(LINES - 3, half_width + 1, 1, 0, ACS_ULCORNER, ACS_RTEE, ACS_LTEE, ACS_RTEE);
+        right = create_newwin(LINES - 3, half_width, 1, half_width, ACS_TTEE, ACS_URCORNER, ACS_BTEE, ACS_RTEE);
+
+        //wattron(left, COLOR_PAIR(1));
+        mvwprintw(left, 0, 2, "%s", " Some dir ");
+        wrefresh(left);
+
+        mvwprintw(right, 0, 2, "%s", " Some another dir ");
+        wrefresh(right);
+        //wattroff(left, COLOR_PAIR(1));
 
     }
 
@@ -39,8 +60,6 @@ class UI
     WINDOW* left;
     WINDOW* right;
     WINDOW* status_bar;
-
-    
 
 
     WINDOW* create_newwin(int height, int width, int starty, int startx, chtype lu_corner = ACS_ULCORNER, chtype ru_corner = ACS_URCORNER, chtype ll_corner = ACS_LLCORNER, chtype rl_corner = ACS_LRCORNER);
