87c6522b309dbcd740ed33774b618a2a73d190cf
Alexey <Garriharripuff@gmail.com>
2023-05-03 16:54:37 +0300

    finishing panel manager

--- src/panel_manager.cpp
+++ src/panel_manager.cpp
@@ -23,17 +23,17 @@ panelManager::panelManager(WINDOW* parent, int height, int width, int starty, in
 
     this->height = height;
     this->width = width;
-    current = subwin(parent, height, width, starty, startx);
+    current = derwin(parent, height, width, starty, startx);
     wbkgd(current, COLOR_PAIR(1));
     smart_wborder(current);
     wrefresh(current);
 
-    tabbar = subwin(parent, 3, width, starty, startx);
+    tabbar = derwin(current, 3, width, 0, 0);
     smart_wborder(tabbar);
     
     wrefresh(tabbar);
 
-    this->tabs.push_back(NEW_TAB);
+    this->tabs.push_back(tab(NEW_TAB, nullptr));
     this->cur_tab = tabs.begin();
 
     registrate_tab(dir.c_str());
@@ -44,7 +44,7 @@ panelManager::panelManager(WINDOW* parent, int height, int width, int starty, in
 
 void panelManager::registrate_tab(std::string name) 
 {
-    tabs.emplace(--this->tabs.end(), name);
+    tabs.emplace(--this->tabs.end(), name, nullptr);
     int tabs_common_size = tabs.size() * TAB_SIZE_MAX;  //there is always selected tab with maximized size
     if (tabs_common_size > width - 1)
     {
@@ -52,7 +52,7 @@ void panelManager::registrate_tab(std::string name)
     }
 }
 
-void panelManager::set_current(std::list<std::string>::iterator tab)
+void panelManager::set_current(std::list<tab>::iterator tab)
 {
     if (tab != tabs.end())
     {
@@ -60,22 +60,39 @@ void panelManager::set_current(std::list<std::string>::iterator tab)
     }
 }
 
-void panelManager::remove_tab(std::list<std::string>::iterator remove)
+void panelManager::remove_tab(std::list<tab>::iterator remove)
 {
     if (remove == tabs.end() || remove == --tabs.end())
         return;
     else if (remove == cur_tab)
         cur_tab = cur_tab++;
 
+    delwin(remove->second);
     this->tabs.erase(remove);
+
+    int tabs_common_size = tabs.size() * TAB_SIZE_MAX;  //there is always selected tab with maximized size
+    if (tabs_common_size > width - 1)
+        tabs_size = (width - TAB_SIZE_MAX - 1) / (tabs.size() - 1);
 
     return;
 }
 
+void panelManager::flush_tabs()
+{
+    for(auto i = tabs.begin(); i != tabs.end(); i++)
+    {
+        i->second = nullptr;
+    }
+
+    destroy_win(tabbar);
+    tabbar = derwin(current, 3, width, 0, 0);
+    smart_wborder(tabbar);
+    wrefresh(tabbar);
+}
+
 void panelManager::redraw_tabs()
 {
-    for (auto i = bars.begin(); i != bars.end(); i++)
-        delete *i;
+    flush_tabs();
 
     int correction = (width - 1 - TAB_SIZE_MAX) % tabs_size;
     int countx = 0;
@@ -84,30 +101,24 @@ void panelManager::redraw_tabs()
         int real_tab_size = tabs_size;
 
         if (i == this->cur_tab)
-        {
             real_tab_size = TAB_SIZE_MAX;
-            bars.emplace_back(derwin(tabbar, 3, real_tab_size + 1, 0, countx));
-            smart_wborder_no_bottom((*bars.rbegin()));
-            wattron((*bars.rbegin()), COLOR_PAIR(1));
-            mvwprintw((*bars.rbegin()), 1, 1, "%s", supress((*i), real_tab_size - 1).c_str());
-            wattroff((*bars.rbegin()), COLOR_PAIR(1));
-            wrefresh((*bars.rbegin()));
-            countx += real_tab_size;
-
-            continue;
-        }
         else if (correction)
         {
             real_tab_size = tabs_size + 1;
             correction--;
         }
 
-        bars.emplace_back(derwin(tabbar, 3, real_tab_size + 1, 0, countx));
-        smart_wborder((*bars.rbegin()));
-        wattron((*bars.rbegin()), COLOR_PAIR(1));
-        mvwprintw((*bars.rbegin()), 1, 1, "%s", supress((*i), real_tab_size - 1).c_str());
-        wattroff((*bars.rbegin()), COLOR_PAIR(1));
-        wrefresh((*bars.rbegin()));
+        i->second = derwin(tabbar, 3, real_tab_size + 1, 0, countx);
+
+        if (i == this->cur_tab)
+            smart_wborder_no_bottom(i->second);
+        else
+            smart_wborder(i->second);
+
+        wattron(i->second, COLOR_PAIR(1));
+        mvwprintw(i->second, 1, 1, "%s", supress(i->first, real_tab_size - 1).c_str());
+        wattroff(i->second, COLOR_PAIR(1));
+        wrefresh(i->second);
         countx += real_tab_size;
     }
 
@@ -115,6 +126,12 @@ void panelManager::redraw_tabs()
     wrefresh(tabbar);
 }
 
+panelManager::~panelManager()
+{
+    flush_tabs();
+
+    destroy_win(current);
+}
 
 
 
--- src/panel_manager.hpp
+++ src/panel_manager.hpp
@@ -7,6 +7,7 @@
 #include <list>
 #include <filesystem>
 #include <cstring>
+#include <tuple>
 
 #include "stuff.hpp"
 
@@ -31,6 +32,7 @@ namespace YAExplorer
 
 extern std::filesystem::path LAUNCH_DIR;
 
+typedef std::pair<std::string, WINDOW*> tab;
 
 class panelManager
 {
@@ -47,10 +49,9 @@ class panelManager
     WINDOW* current;
     WINDOW* tabbar;
 
-    std::list<std::string> tabs; //defenetly bad idea
-    std::list<WINDOW*> bars;
+    std::list<tab> tabs;
 
-    std::list<std::string>::iterator cur_tab;
+    std::list<tab>::iterator cur_tab;
 
     int height;
     int width;
@@ -59,20 +60,15 @@ class panelManager
 
 
     void registrate_tab(std::string name);                      //that thing only REGISTER, NOT DRAWING new tab, only for internal class use
-    void remove_tab(std::list<std::string>::iterator removed);  //most likely deleting tab will be current chosen, os iterator will be good idea
-    void set_current(std::list<std::string>::iterator);
+    void remove_tab(std::list<tab>::iterator removed);  //most likely deleting tab will be current chosen, os iterator will be good idea
+    void set_current(std::list<tab>::iterator);
     void redraw_tabs();                                         //as evety possible change will defenitly cause all tabs structure is easiear to reedraw all tabs, only for internal use
+    void flush_tabs();
 
 
     public:
 
-    ~panelManager()
-    {
-        wborder(current, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-	    wrefresh(current);
-	    delwin(current);
-    }
-
+    ~panelManager();
 };
 
 };
