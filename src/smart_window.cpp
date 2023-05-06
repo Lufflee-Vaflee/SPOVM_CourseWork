5c3b02864e50f067812ed49a5db493a1ccfbc693
Alexey <Garriharripuff@gmail.com>
2023-05-06 08:39:38 +0300

    smartWindow body

--- src/smart_window.cpp
+++ src/smart_window.cpp
@@ -2,7 +2,275 @@
 
 namespace YAExplorer
 {
-    const std::shared_ptr<smartWindow> smartWindow::main_ptr = std::shared_ptr<smartWindow>(new smartWindow());
 
-    
+const std::shared_ptr<smartWindow> smartWindow::main = std::shared_ptr<smartWindow>(new smartWindow());
+
+const std::weak_ptr<smartWindow> smartWindow::main_ptr = main;
+
+smartWindow::smartWindow()      //special constructor for main window
+{
+    this->raw = stdscr;
+    this->neighbours = list<weak_ptr<smartWindow>>();
+    this->selfref = main;
+}
+
+smartWindow::smartWindow(int x, int y, int width, int height, weak_ptr<smartWindow> parent, const list<weak_ptr<smartWindow>>& neighbours)
+{
+    if(auto p = parent.lock())
+        this->raw = derwin(p->raw, height, width, y, x);
+    this->neighbours = neighbours;
+}
+
+smartWindow::smartWindow(smartWindow&& other)
+{
+    this->raw = other.raw;
+    other.raw = nullptr;
+    this->neighbours = other.neighbours;
+    other.neighbours.clear();
+    this->heirs = other.heirs;
+    other.heirs.clear();
+}
+
+int smartWindow::getX()
+{
+    return getbegx(this->raw);  
+}
+
+int smartWindow::getY()
+{
+    return getbegy(this->raw);  
+}
+
+int smartWindow::get_width()   //https://linux.die.net/man/3/getbegyx
+{
+    return getmaxx(this->raw);
+}
+
+int smartWindow::get_height()
+{
+    return getmaxy(this->raw);
+}
+
+int smartWindow::get_maxX()
+{
+    return this->getX() +this->get_width() - 1;
+}
+
+int smartWindow::get_maxY()
+{
+    return this->getY() +this->get_height() - 1;
+}
+
+XY smartWindow::getXY()
+{
+    return XY(this->getX(), this->getY());
+}
+
+XY smartWindow::getWH()
+{
+    return XY(this->get_width(), this->get_height());
+}
+
+XY smartWindow::getmaxXY()
+{
+    return XY(this->get_maxX(), this->get_maxY());
+}
+
+chtype smartWindow::getcorner(int x, int y, bool U, bool B, bool L, bool R)
+{
+
+    U = (mvinch(y - 1, x) == (ACS_VLINE | COLOR_PAIR(1))) || U;
+    B = (mvinch(y + 1, x) == (ACS_VLINE | COLOR_PAIR(1))) || B;
+    L = (mvinch(y, x - 1) == (ACS_HLINE | COLOR_PAIR(1))) || L;
+    R = (mvinch(y, x + 1) == (ACS_HLINE | COLOR_PAIR(1))) || R;
+
+    int flags = 0;
+    chtype result;
+
+    flags += (int)U;
+    flags <<= 1;
+    flags += (int)B;
+    flags <<= 1;
+    flags += (int)L;
+    flags <<= 1;
+    flags += (int)R;
+
+    switch (flags)
+    {
+    case 0b1111:
+        result = ACS_PLUS;
+        break;
+    case 0b0111:
+        result = ACS_TTEE;
+        break;
+    case 0b1011:
+        result = ACS_BTEE;
+        break;
+    case 0b1101:
+        result = ACS_LTEE;
+        break;
+    case 0b1110:
+        result = ACS_RTEE;
+        break;
+    case 0b0101:
+        result = ACS_ULCORNER;
+        break;
+    case 0b0110:
+        result = ACS_URCORNER;
+        break;
+    case 0b1001:
+        result = ACS_LLCORNER;
+        break;
+    case 0b1010:
+        result = ACS_LRCORNER;
+        break;
+    case 0b1100:
+        result = ACS_VLINE;
+        break;
+    case 0b0011:
+        result = ACS_HLINE;
+        break;
+    default:
+        return 0;
+        break;
+    }
+
+    return result;
+}
+
+void smartWindow::draw_borders()
+{
+    chtype ul = getcorner(this->getX(), this->getY(), false, true, false, true);
+    chtype ur = getcorner(this->get_maxX(), this->getY(), false, true, true, false);
+    chtype bl = getcorner(this->getX(), this->get_maxY(), true, false, false, true);
+    chtype br = getcorner(this->get_maxX(), this->get_maxY(), true, false, true, false);
+
+    wattron(this->raw, COLOR_PAIR(1));                   //defenitly need to do something with this hardcoded colors
+    wborder(this->raw, 0, 0, 0, 0, ul, ur, bl, br);
+    wattroff(this->raw, COLOR_PAIR(1));
+
+    if(this->auto_refresh)
+        wrefresh(this->raw);
+}
+
+void smartWindow::draw_borders_no_bottom()
+{
+    wattron(this->raw, COLOR_PAIR(1));
+    wborder(this->raw, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+    wattroff(this->raw, COLOR_PAIR(1));
+
+    chtype ul = getcorner(this->getX(), this->getY(), false, true, false, true);
+    chtype ur = getcorner(this->get_maxX(), this->getY(), false, true, true, false);
+    chtype bl = getcorner(this->getX(), this->get_maxY(), true, false, false, false);
+    chtype br = getcorner(this->get_maxX(), this->get_maxY(), true, false, false, false);
+
+    wattron(this->raw, COLOR_PAIR(1));
+    wborder(this->raw, 0, 0, 0, ' ', ul, ur, bl, br);
+    wattroff(this->raw, COLOR_PAIR(1));
+
+    if(this->auto_refresh)
+        wrefresh(this->raw);
+}
+
+void smartWindow::delete_borders(bool smart)
+{
+    chtype ul = getcorner(this->getX(), this->getY(), false, true, false, true);
+    chtype ur = getcorner(this->get_maxX(), this->getY(), false, true, true, false);
+    chtype bl = getcorner(this->getX(), this->get_maxY(), true, false, false, true);
+    chtype br = getcorner(this->get_maxX(), this->get_maxY(), true, false, true, false);
+
+    wattron(this->raw, COLOR_PAIR(1));
+	wborder(this->raw, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+    wattroff(this->raw, COLOR_PAIR(1));
+
+    if (smart)
+    {
+        for(auto i = this->neighbours.begin(); i != this->neighbours.end(); i++)
+        {
+            auto temp = (*i).lock();
+
+            if (temp)
+            {
+                temp->draw_borders();
+                temp->refresh();
+            }
+            else
+            {
+                auto del = i;
+                i--;
+                this->neighbours.erase(del);
+            }
+        }
+    }
+
+    if (this->auto_refresh)
+        wrefresh(this->raw);
+}
+
+void smartWindow::flush()
+{
+    wclear(this->raw);
+    wbkgd(this->raw, COLOR_PAIR(1));
+
+    if(this->auto_refresh)
+        wrefresh(this->raw);
+}
+
+string smartWindow::supress(string str, unsigned int to)
+{
+    if(str.size() <= to)
+        return str;
+
+    int overflow = str.size() - to;
+    overflow = 2; 
+
+    std::string result("");
+    int first_part_size = (to - overflow) / 2;
+    int second_part_size = (to - overflow) - first_part_size;
+    result += str.substr(0, first_part_size);
+
+    result += "..";
+
+    result += str.substr(str.size() - second_part_size, second_part_size);
+
+    return result;
+}
+
+void smartWindow::print(const std::string& mes)
+{
+    wattron(this->raw, COLOR_PAIR(1));
+    mvwprintw(this->raw, 1, 1, "%s", this->supress(mes, this->get_width() - 2));
+    wattroff(this->raw, COLOR_PAIR(1));
+}
+
+void smartWindow::refresh()
+{
+    wrefresh(this->raw);
+}
+
+void smartWindow::registrate_neighbour(weak_ptr<smartWindow> neighbour)
+{
+    this->neighbours.emplace_back(neighbour);
+}
+
+weak_ptr<smartWindow> smartWindow::create_heir(int x, int y, int width, int height, const list<weak_ptr<smartWindow>>& neighbours)
+{
+    if (width < MIN_WINDOW_WIDTH || height < MIN_WINDOW_HEIGHT)
+        throw exception();
+    // add other checks;
+
+    shared_ptr<smartWindow> new_one = shared_ptr<smartWindow>(new smartWindow(x, y, width, height, this->selfref, neighbours));
+    new_one->selfref = new_one;
+    return new_one;
+}
+
+smartWindow::~smartWindow()
+{
+    this->heirs.clear();
+    wclear(this->raw);
+    this->delete_borders(true);
+    delwin(this->raw);
+}
+
+
 }
--- src/smart_window.hpp
+++ src/smart_window.hpp
@@ -2,44 +2,55 @@
 
 #include "stuff.hpp"
 #include <ncurses.h>
-#include <vector>
+#include <list>
 #include <functional>
 #include <iostream>
 #include <memory>
 
-#define MIN_WINDOW_HEIGHT 3;
-#define MIN_WINDOW_WIDTH 3;
+#define MIN_WINDOW_HEIGHT 3
+#define MIN_WINDOW_WIDTH 3
 
 namespace YAExplorer
 {
 
 class smartWindow;
+
 typedef smartWindow SWindow;
+
+typedef std::pair<int, int> XY;
 
 using namespace std;
 
 class smartWindow
 {
-    private:
-
-    smartWindow()       //special constructor for main window
-    {
-
-    }
-
     public:
 
-    smartWindow(int x, int y, int width, int height, weak_ptr<smartWindow> parent = main_ptr, const vector<weak_ptr<smartWindow>>& neighbours = std::vector<weak_ptr<smartWindow>>())
-    {
-        
-    }
-
-    static const shared_ptr<smartWindow> main_ptr;
+    static const weak_ptr<smartWindow> main_ptr;
 
     private:
 
-    vector<weak_ptr<smartWindow>> neighbours;
-    shared_ptr<smartWindow> current;
+    static const shared_ptr<smartWindow> main;
+
+    smartWindow();      //special constructor for main window
+
+    smartWindow(int x, int y, int width, int height, weak_ptr<smartWindow> parent = main, const list<weak_ptr<smartWindow>>& neighbours = list<weak_ptr<smartWindow>>());
+
+    smartWindow(smartWindow &other) = delete;
+    void operator=(const smartWindow&) = delete;
+
+    smartWindow(smartWindow&& other);
+
+    bool auto_refresh = true;
+
+    list<weak_ptr<smartWindow>> neighbours;
+    list<shared_ptr<smartWindow>> heirs = list<shared_ptr<smartWindow>>();
+
+    weak_ptr<smartWindow> selfref;
+
+    WINDOW* raw; //raw window pointer to interract with ncurses lib
+
+    chtype getcorner(int x, int y, bool U, bool B, bool L, bool R);
+    std::string supress(std::string str, unsigned int to);
 
     public:
 
@@ -47,18 +58,28 @@ class smartWindow
     int getY();
     int get_width();
     int get_height();
-    int get_maxX();
+    int get_maxX(); 
     int get_maxY();
+    XY  getXY();
+    XY  getWH();
+    XY  getmaxXY();
 
     void draw_borders();
-    void delete_borders();
+    void draw_borders_no_bottom();
+    void delete_borders(bool smart = true);
+    void flush();
 
+    void refresh();
+    void print(const std::string& mes); // add more complex parameters
 
-    ~smartWindow()
-    {
+    void set_auto_refresh(bool refresh);
 
-    }
+    void registrate_neighbour(weak_ptr<smartWindow> neighbour);
+    bool delete_neighbour(weak_ptr<smartWindow> neighbour);
 
+    weak_ptr<smartWindow> create_heir(int x, int y, int width, int height, const list<weak_ptr<smartWindow>>& neighbours = list<weak_ptr<smartWindow>>());
+
+    ~smartWindow();
 };
 
 
